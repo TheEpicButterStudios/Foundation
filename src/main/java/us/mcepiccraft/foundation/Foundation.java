cfc02d1f2ac068a9dd864d0badfe03fcdbc6ff63
TheEpicButterStudios <samuel.bird4215@gmail.com>
2014-09-19 15:20:03 -0500

    Let's get this started.

---
+++ src/main/java/us/mcepiccraft/foundation/Foundation.java
@@ -0,0 +1,16 @@
+package us.mcepiccraft.foundation;
+
+@Plugin(id="Foundation", name="Foundation", version="1.0.0")
+public class Foundation {
+	@SpongeEventHandler
+	public void onEnable(ServerStartingEvent e)
+	{
+		//TODO Add code when API becomes stable
+	}
+	@SpongeEventHandler
+	public void onDisable(ServerStoppingEvent e)
+	{
+		//TODO Add code when API becomes stable
+	}
+
+}
